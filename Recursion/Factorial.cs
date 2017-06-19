a605dcb90981973fa7c4502e611911ea14dd117e
Mariusz Maciejczuk <mariusz.maciejczuk@corp.smtsoftware.com>
2017-06-19 13:51:26 +0200

    first commit

---
+++ Recursion/Factorial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    public static class Factorial
+    {
+        public static int FactorialFunc(int factorialNum)
+        {
+            if (factorialNum == 1)
+                return 1;
+            return factorialNum * FactorialFunc(factorialNum - 1);
+
+            // Factorial(5) = 5 * Factorial(4)
+            // Factorial(4) = 4 * Factorial(3)
+            // Factorial(3) = 4 * Factorial(2)
+            // Factorial(2) = 4 * Factorial(1)
+            // Factorial(1) = 1;
+        }
+
+        public static float Power(float num, int powerOf)
+        {
+            // Exit condition
+            if (powerOf == 1)
+                return num;
+            return num * Power(num, powerOf - 1);
+
+        }
+    }
+}
---
+++ Recursion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Factorial.FactorialFunc(5);
+            Console.WriteLine($"Factorial of 5 is : {result}");
+            Console.ReadKey();
+
+            float result2 = Factorial.Power(5, 4);
+            Console.WriteLine($"Power of 5 to 4 is : {result2}");
+            Console.ReadKey();
+
+        }
+    }
+}
